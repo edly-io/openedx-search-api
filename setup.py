89b9a62c1633b750b1ebe3363ec50645165feea5
qasimgulzar <qasim.khokhar52@gmail.com>
2024-07-15 14:17:33 +0500

    init: 🔥

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-search',
+    version='0.0.0',
+    packages=['search', 'search.drivers', 'search.migrations', 'django_search'],
+    url='https://github.com/qasimgulzar/django-search',
+    license='',
+    author='qasimgulzar',
+    author_email='qasim.khokhar52@gmail.com',
+    description=''
+)
