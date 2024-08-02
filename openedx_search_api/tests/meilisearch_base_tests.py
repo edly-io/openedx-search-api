a31423ab0f5ab56768624f5b08f17c9c36dae433
qasimgulzar <qasim.khokhar52@gmail.com>
2024-08-02 14:43:22 +0500

    fix: quality

--- openedx_search_api/tests/meilisearch_base_tests.py
+++ openedx_search_api/tests/meilisearch_base_tests.py
@@ -8,8 +8,8 @@ class MeilisearchDriverTestCase(TestCase):
     def test_meilisearch_connection(self):
         client = MeiliSearchEngine(
             None,
-            "http://meilisearch:42069",
-            "http://meilisearch:42069",
+            "http://meilisearch:7700",
+            "http://meilisearch:7700",
             "c471bd40-303b-48bc-bb80-4602baff2675",
             "7d6b4e462ac450051d0618d277dc46051db30160bf9bc28a0dd2e7f097c9eecd",
             "masterKey",
